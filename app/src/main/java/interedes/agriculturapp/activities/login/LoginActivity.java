62d04272905f67a9aeab3511329a946b03b32587
enu-44 <enuarmunozcastillo@gmail.com>
2017-12-27 08:57:48 -0500

    Changes Register User, Strings, Colors, Styles

--- app/src/main/java/interedes/agriculturapp/activities/login/LoginActivity.java
+++ app/src/main/java/interedes/agriculturapp/activities/login/LoginActivity.java
@@ -1,9 +1,11 @@
 package interedes.agriculturapp.activities.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import interedes.agriculturapp.R;
+import interedes.agriculturapp.activities.register_user.RegisterUserActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +13,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
