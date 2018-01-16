ba2084d3944927921e1b5c7864b57635fdf40403
davidlambis <davidlambis261@gmail.com>
2018-01-16 16:13:01 -0500

    cultivos, ventas y diálogos Productor

---
+++ app/src/main/java/interedes/agriculturapp/activities/productor/MisVentasProductorActivity.java
@@ -0,0 +1,16 @@
+package interedes.agriculturapp.activities.productor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import interedes.agriculturapp.R;
+
+
+public class MisVentasProductorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_ventas_productor);
+    }
+}
