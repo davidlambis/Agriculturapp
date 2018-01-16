b5c6485bca439f27091eac7f06045c190d3954fe
davidlambis <davidlambis261@gmail.com>
2018-01-16 17:54:40 -0500

    Layout Ofertas,Ventas, Cultivos Productor

---
+++ app/src/main/java/interedes/agriculturapp/activities/productor/OfertasProductorActivity.java
@@ -0,0 +1,15 @@
+package interedes.agriculturapp.activities.productor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import interedes.agriculturapp.R;
+
+public class OfertasProductorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ofertas_productor);
+    }
+}
