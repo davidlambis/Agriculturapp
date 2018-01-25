d6c1e148fae2ec5fce3e7feb2b725cc57dd8e183
davidlambis <davidlambis261@gmail.com>
2018-01-25 14:39:18 -0500

    AgroInsumos

---
+++ app/src/main/java/interedes/agriculturapp/activities/productor/agroinsumos_productor/AgroinsumosProductorActivity.java
@@ -0,0 +1,12 @@
+package interedes.agriculturapp.activities.productor.agroinsumos_productor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgroinsumosProductorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
